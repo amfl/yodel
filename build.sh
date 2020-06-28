b35379465e706f6985d1e920ce14805e12a2722a
amfl <amfl@users.noreply.github.com>
2020-06-28 17:42:20 +1200

    Add Github Actions (#3)

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 go build -v -o build/yodel ./cmd/yodel
+# http://blog.wrouesnel.com/articles/Totally%20static%20Go%20builds/
+# CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static"' .
