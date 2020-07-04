16e0af996af7a074692aef80824534823062bcd7
amfl <amfl@none.none>
2020-07-04 20:08:38 +1200

    Add table output with go-pretty

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/deckarep/golang-set v1.7.1
 	github.com/go-ldap/ldap v3.0.3+incompatible
+	github.com/jedib0t/go-pretty/v6 v6.0.4
 	github.com/spf13/viper v1.7.0
 	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
 	gopkg.in/yaml.v2 v2.2.4
--- internal/output.go
+++ internal/output.go
@@ -1,8 +1,10 @@
 package yodel
 
 import (
-	mapset "github.com/deckarep/golang-set"
 	"gopkg.in/yaml.v2"
+
+	mapset "github.com/deckarep/golang-set"
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
 // OutputYaml outputs a set of groups as yaml :)
@@ -17,4 +19,20 @@ func OutputYaml(groups mapset.Set, annotate func(string) string) (string, error)
 	}
 
 	return string(d), nil
+}
+
+// OutputTable outputs a set of groups as a table with go-pretty
+func OutputTable(groups mapset.Set) (string, error) {
+	// Convert to slice
+	slice := groups.ToSlice()
+
+	tw := table.NewWriter()
+
+	rows := make([]table.Row, len(slice))
+	for i, r := range slice {
+		rows[i] = table.Row{r}
+	}
+	// Append a header
+	tw.AppendRows(rows)
+	return tw.Render(), nil
 }
